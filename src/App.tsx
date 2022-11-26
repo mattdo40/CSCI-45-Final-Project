6b2ad9a08888d791dee1026262286b050f8e2abd
Matt <Matt@PC>
2022-11-26 13:56:51 -0800

    added Matt's part to Matt's

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 import GameBoard from 'src/components/GameBoard';
 import React from 'react';
-
+import Game from "./game/Game";
 const App: React.FunctionComponent = ():JSX.Element => {
 
   return (
     <div>
       <h1>Connect Four</h1>
       <GameBoard/>
+      <Game />
     </div>
+
   );
 }
 
